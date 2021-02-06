8e9e479a4ad9e077cfe55cfd61536df6d47af3a6
SANTHOSHKUMARD <31656855+Santhoshkumard11@users.noreply.github.com>
2021-02-06 11:44:09 +0530

    added models

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,12 @@ namespace LearnTodayWebAPI.Models
 {
     public class Student
     {
-
+        [Key]
         public int EndrolemnentId { get; set; }
         public int StudentId { get; set; }
         public int CourseId { get; set; }
 
         //navigation property
-        public List<Course> CourseList { get; set; }
+        public Course Course { get; set; }
     }
 }
---
+++ Models/Trainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnTodayWebAPI.Models
+{
+    public class Trainer
+    {
+        public int TrainerId { get; set; }
+        public String Password { get; set; }
+
+    }
+}
