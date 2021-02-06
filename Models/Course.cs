4093880f73afb985f64cb2c7733cba8f4a9d53de
SANTHOSHKUMARD <31656855+Santhoshkumard11@users.noreply.github.com>
2021-02-06 11:30:20 +0530

    Add project files.

---
+++ Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnTodayWebAPI.Models
+{
+    public class Course
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public float Fees { get; set; }
+        public String Description { get; set; }
+        public string Trainer { get; set; }
+        public DateTime StartDate { get; set; }
+        //navigation property
+        public List<Student> StudentsList { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnTodayWebAPI.Models
+{
+    public class Student
+    {
+
+        public int EndrolemnentId { get; set; }
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+
+        //navigation property
+        public List<Course> CourseList { get; set; }
+    }
+}
